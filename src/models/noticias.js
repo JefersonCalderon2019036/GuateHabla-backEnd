a3df8b62127d24d1e2633ceb018077fa6c3cca26
ndelcid2016535 <69643109+ndelcid-2016535@users.noreply.github.com>
2021-07-22 18:50:52 -0600

    Add files via upload

---
+++ src/models/noticias.js
@@ -0,0 +1,17 @@
+'use strict'
+
+var mongoose = require('mongoose')
+var Schema = mongoose.Schema;
+
+var noticiasSchema = Schema({
+    titulo: String,
+    descripcion: String,
+    fechaCreacion: String,
+    img: String,
+    comentarios: [{
+        userIdComentario: {type: Schema.Types.ObjectId, ref: 'User'},
+        descripcionComentario: String
+    }]
+})
+
+module.exports = mongoose.model('Noticias', noticiasSchema)
